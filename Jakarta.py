bddb0c840c9a5ba880cca6d487cb9b7b59b29c84
pebridayanti <pebridayantihasibuan@gmail.com>
2017-10-29 01:18:02 +0700

    Update Jakarta.py

--- Jakarta.py
+++ Jakarta.py
@@ -23,3 +23,25 @@ folium.Marker([-6.194941, 106.845219], popup='<i>Kenari</i>').add_to(pebridayant
 folium.Marker([-6.261920, 106.831258], popup='<i>Kalibata</i>').add_to(pebridayanti)
 folium.Marker([-6.223247, 106.911958], popup='<i>12</i>').add_to(pebridayanti)
 folium.Marker([-6.223487, 106.910783], popup='<i>Jl.Duren Sawit Indah Bblok A7 No.4</i>').add_to(pebridayanti)
+
+import folium
+
+pebridayanti = folium.Map (
+    location=[-6.244907, 106.846502], 
+    zoom_start=12,
+    tiles='Stamen Terrain'
+)
+
+tooltip = 'Click me!'
+
+
+folium.Marker([-6.243468, 106.845440], popup='<i>2</i>').add_to(pebridayanti)
+folium.Marker([-6.243937, 106.845107], popup='<i>Jakarta Selatan</i>').add_to(pebridayanti)
+folium.Marker([-6.245643, 106.844485], popup='<i>Jl.Pancoran Buntu 1 No.30</i>').add_to(pebridayanti)
+folium.Marker([-6.246128, 106.847403], popup='<i>Gudang Sarinah</i>').add_to(pebridayanti)
+folium.Marker([-6.246294, 106.846299], popup='<i>Jaya Obayashi</i>').add_to(pebridayanti)
+folium.Marker([-6.245801, 106.846363], popup='<i>2</i>').add_to(pebridayanti)
+folium.Marker([-6.246038, 106.847063], popup='<i>Serrum Studio</i>').add_to(pebridayanti)
+folium.Marker([-6.245715, 106.847524], popup='<i>9</i>').add_to(pebridayanti)
+folium.Marker([-6.245838, 106.847521], popup='<i>Unnamed Road</i>').add_to(pebridayanti)
+folium.Marker([-6.245766, 106.847229], popup='<i>Forumsinema</i>').add_to(pebridayanti)
